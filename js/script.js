2c78a3c21a23227787c2303b2cfb9e2f48f15551
Riki420 <girottiriccardo1998@gmail.com>
2021-07-28 15:47:04 +0200

    immagini inserite una affianco all'altra

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 console.log(Vue);
 
+const root = new Vue({
+    el: '#root',
+    data: { 
+        images: [
+            'img/image1.jpg',
+            'img/image2.jpg',
+            'img/image3.jpg',
+            'img/image4.jpg',
+
+        ],
+    },
+    methods: {}
+})
