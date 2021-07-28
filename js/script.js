75f53dfd60a02897e2bfd10865cc965edc50b0a0
Riki420 <girottiriccardo1998@gmail.com>
2021-07-28 17:22:41 +0200

    bottoni funzionanti

--- js/script.js
+++ js/script.js
@@ -15,6 +15,20 @@ const root = new Vue({
     methods: {
         isActive(index){
             return this.currentIndex === index ? 'active' : '';             //grazie a questa funzione vediamo se è presente l'active
+        },
+        increaseIndex(){                                                    //funzione per incrementare l'index dell'immagine
+            if(this.cuerrentIndex === this.images.length - 1){               //se il currentIndex raggiunge la lunghezza massima dell'array torna al primo elemento
+                this.currentIndex = 0;
+            } else{
+                this.currentIndex++;
+            }
+        },
+        decreaseIndex(){
+            if(this.cuerrentIndex === 0){                                   //Il currentIndex non può andare sotto zero
+                this.currentIndex = this.images.length - 1;
+            } else{
+                this.currentIndex--;
+            }
         }
     }
 })
